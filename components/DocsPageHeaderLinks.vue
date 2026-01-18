e22180856556e3c716cca8d6d9ef74dc18bbdeba
Matvey Melishev <melishevm@gmail.com>
2026-01-19 00:03:03 +0100

    feat: add LastUpdated component to display last update date from GitHub

--- components/DocsPageHeaderLinks.vue
+++ components/DocsPageHeaderLinks.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineOgImageComponent('OgImage')
-  
+
 const route = useRoute()
 
 const { data: page } = await useAsyncData(route.path, () => {
@@ -9,17 +9,20 @@ const { data: page } = await useAsyncData(route.path, () => {
 </script>
 
 <template>
-  <div class="flex w-full items-center gap-5">
-    <UUser
-      v-for="author in page.meta.authors"
-      :key="author.name"
-      :name="author.name"
-      :avatar="{
+  <div class="flex flex-col sm:flex-row w-full sm:items-center gap-5 justify-between">
+    <div class="flex items-center gap-3">
+      <UUser v-for="author in page.meta.authors" :key="author.name" :name="author.name" :avatar="{
         src: author.avatar.src,
         icon: 'i-lucide-user',
-      }"
-      :to="author?.to"
-      target="_blank"
-    />
+      }" :to="author?.to" target="_blank" />
+    </div>
+
+    <LastUpdated />
   </div>
 </template>
+
+<style>
+div[data-slot="links"] {
+  width: 100%;
+}
+</style>
